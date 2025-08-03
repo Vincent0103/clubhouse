d1983fb2553e73da4f0cddaac8fc38efac6d3cb4
Vincent0103 <vincentkanagasundaram@gmail.com>
2025-08-03 12:21:22 +0200

    Improve register field validators

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -3,52 +3,55 @@ import db from "../db/queries";
 const { body, validationResult } = require("express-validator");
 
 const alphaErr = "must only contain letters.";
-const usernameErr = "must be between 3 and 255 characters.";
+const lengthErr = (min, max) => `must be between ${min} and ${max} characters.`;
 const emailErr = 'must be in valid format: "hello@example.com".';
-const passwordLengthErr = "be between 8-128 characters";
 const lowerCaseErr = "have atleast one lowercase letter (a-z)";
 const upperCaseErr = "have atleast one uppercase letter (A-Z)";
 const digitErr = "have atleast one digit (0-9)";
 const specialCharErr = "have atleast one special character (@$!%*?&)";
 const matchPasswordErr = "do not match.";
 
-const passwordValidationRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]/;
+const passwordValidationRegex =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]+$/;
+
 const validateRegister = [
   body("firstName")
     .trim()
     .isAlpha()
+    .withMessage(`First name ${alphaErr}`)
     .isLength({ min: 3, max: 255 })
-    .withMessage(`First name ${alphaErr} and ${usernameErr}`),
+    .withMessage(`First name ${lengthErr(3, 255)}`),
   body("lastName")
     .trim()
     .isAlpha()
+    .withMessage(`Last name ${alphaErr}`)
     .isLength({ min: 3, max: 255 })
-    .withMessage(`Last name ${alphaErr} and ${usernameErr}`),
+    .withMessage(`Last name ${lengthErr(3, 255)}`),
   body("username")
     .trim()
     .isLength({ min: 3, max: 255 })
-    .withMessage(`Username ${usernameErr}`),
-  body("email").trim().isEmail().withMessage(`Email ${emailErr}`),
-  body("password")
+    .withMessage(`Username ${lengthErr(3, 255)}`),
+  body("mail").trim().isEmail().withMessage(`Email ${emailErr}`),
+  body("pwd")
     .trim()
     .isLength({ min: 8, max: 128 })
+    .withMessage(`Password ${lengthErr(8, 128)}`)
     .matches(passwordValidationRegex)
     .withMessage([
       "Password must:",
-      `- ${passwordLengthErr}`,
-      `- ${lowerCaseErr}`,
-      `- ${upperCaseErr}`,
-      `- ${digitErr}`,
-      `- ${specialCharErr}`,
+      `• ${lowerCaseErr}`,
+      `• ${upperCaseErr}`,
+      `• ${digitErr}`,
+      `• ${specialCharErr}`,
     ]),
-  body("confirmPassword")
+  body("confirmPwd")
     .trim()
     .custom((value, { req }) => {
       // If the main password is not valid don't handle errors in here
-      if (!req.body.password.match(passwordValidationRegex)) {
+      if (!req.body.pwd.match(passwordValidationRegex)) {
         return true;
       }
-      if (value !== req.body.password) {
+      if (value !== req.body.pwd) {
         throw new Error(`Passwords ${matchPasswordErr}`);
       }
       return true;
@@ -71,14 +74,15 @@ const indexController = (() => {
   const registerPost = [
     validateRegister,
     (req, res) => {
+      console.log(Object.keys(req.body));
       const errors = validationResult(req);
       if (!errors.isEmpty()) {
-        return res.status(404).render("register", {
+        return res.status(400).render("register", {
           errors: errors.array(),
         });
       }
-      const { firstName, lastName, username, email, password } = req.body;
-      db.createUser({ firstName, lastName, username, email, password });
+      const { firstName, lastName, username, mail, pwd } = req.body;
+      db.createUser({ firstName, lastName, username, mail, pwd });
       res.redirect("/");
     },
   ];
